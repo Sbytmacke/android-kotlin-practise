39261dcbe591ad2e4e59e09e66911c70b1396c04
Angel M.Chivite <sbytmacke@gmail.com>
2023-09-14 23:03:02 +0200

    [Add] (FirstApp with layouts linear & constraint)

---
+++ FirstApp/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,17 @@
+
+z
+Japp/src/main/java/dev/sbytmacke/firstapp/ui/slideshow/SlideshowFragment.kt,f\d\fd646127867505474f6b008b78b89bffba3e9db7
+v
+Fapp/src/main/java/dev/sbytmacke/firstapp/ui/gallery/GalleryFragment.kt,3\5\354e62ef03174f8111ac3883da37cdd390ff7099
+p
+@app/src/main/java/dev/sbytmacke/firstapp/ui/home/HomeFragment.kt,9\f\9f8d283e9a110eb3657c7ccfbca4a26707230033
+q
+Aapp/src/main/java/dev/sbytmacke/firstapp/ui/home/HomeViewModel.kt,6\d\6dcede536a73bbd78be3d56c2308ce7188a09998
+s
+Capp/src/main/java/dev/sbytmacke/firstapp/ui/login/LoginViewModel.kt,4\2\42d29301069468a8e6d371e30f6622ef10c2b694
+h
+8app/src/main/java/dev/sbytmacke/firstapp/MainActivity.kt,7\6\76a12f37865cecb288189a61d3932cee76c8b038
+r
+Bapp/src/main/java/dev/sbytmacke/firstapp/ui/login/LoginFragment.kt,c\e\ce7152bb482d91a942571289cb0271db1e348d6e
+x
+Happ/src/main/java/dev/sbytmacke/firstapp/ui/login/LoginLinearFragment.kt,8\9\89cfe62540febc5864cea9626499475fdea99af4
---
+++ FirstApp/app/src/main/java/dev/sbytmacke/firstapp/ui/login/LoginLinearFragment.kt
@@ -0,0 +1,50 @@
+package dev.sbytmacke.firstapp.ui.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import dev.sbytmacke.firstapp.R
+import dev.sbytmacke.firstapp.databinding.FragmentHomeBinding
+
+class LoginLinearFragment : Fragment() {
+
+    // TODO: Como cambio el fragment de HomeBinding a  LoginFragment??
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val loginViewModel =
+            ViewModelProvider(this).get(LoginViewModel::class.java)
+
+        // El inflater es el layout?
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.login_main_linear, container, false)
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textHome
+        loginViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+
+        // Retornamos root o view
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
